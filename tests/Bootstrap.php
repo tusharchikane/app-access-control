f33638b1722e820544f05fc70232f73ae7135da1
neilime <escalle.emilien@gmail.com>
2013-04-30 11:51:14 +0300

    Update Bootstrap.php

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 namespace BoilerAppAccessControlTest;
 error_reporting(E_ALL | E_STRICT);
+ini_set ('max_execution_time', 0);
 chdir(dirname(__DIR__));
 if(is_readable($sBoilerAppTestBootstrapPath = __DIR__.'/../vendor/zf2-boiler-app/app-test/src/BoilerAppTest/AbstractBootstrap.php'))include $sBoilerAppTestBootstrapPath;
 if(!class_exists('BoilerAppTest\AbstractBootstrap'))throw new \RuntimeException('Unable to load BoilerAppTest Bootstrap. Install required libraries through `composer`');
